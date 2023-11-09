0b859868a09fe67b1a6397f7a9609846cb09fae7
Dominic Digby <domdigby@gmail.com>
2023-11-09 09:46:54 +0000

    Challenge 23 - area of turf for lawn

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def car_discount():
     # Get the retail cost
     retail_cost = input("Enter the retail cost of the car: ")
@@ -44,6 +47,35 @@ def speed_distance_time():
     print(f"The desired {calc_dec} will be: {calc} {units}")
 
 
+def area_calculation():
+    """
+    Challenge 23:  A gardener needs to buy some turf for a project
+    they are working on. The garden is rectangular
+    with a circular flower bed in the middle.
+    :return:
+    """
+    # Ask for user inputs - catch non number
+    try:
+        lawn_length = float(input("Enter the lawn length in metres: "))
+        lawn_width = float(input("Enter the lawn width in metres: "))
+        bed_radius = float(input("Enter the flower bed radius in metres: "))
+    except ValueError as e:
+        print(f"{e.args[0]} was not a valid number.  Please retry")
+        return
+
+    # Calculate the area of the rectangle
+    lawn_area = lawn_length * lawn_width
+
+    # Calculate the area of the flower beb
+    flower_bed = math.pi * bed_radius * bed_radius
+
+    # Subtract the flower bed from the lawn area for the turf required
+    lawn_area = lawn_area - flower_bed
+
+    print(f"{lawn_area:.2f} m2 of turf is required for a lawn {lawn_length} m by {lawn_width} m, with a flower bed of {bed_radius} m radius")
+
+
 if __name__ == '__main__':
     # car_discount()
-    speed_distance_time()
+    #speed_distance_time()
+    area_calculation()
